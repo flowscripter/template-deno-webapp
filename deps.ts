89e90ba31c792669836b28e84d0599855b50012b
vectronic <hello.vectronic@gmail.com>
2022-06-10 02:46:33 +0100

    chore: update dependencies (#15)
    Co-authored-by: vectronic <vectronic@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as log from "https://deno.land/std@0.142.0/log/mod.ts";
-export * as denoLib from "https://raw.githubusercontent.com/flowscripter/template-deno-library/v1.2.0/mod.ts";
+export * as log from "https://deno.land/std@0.143.0/log/mod.ts";
+export * as denoLib from "https://raw.githubusercontent.com/flowscripter/template-deno-library/v1.3.0/mod.ts";
 export * as wasmLib from "https://flowscripter.github.io/template-wasm-rust-library/flowscripter_template_wasm_rust_library.js";
